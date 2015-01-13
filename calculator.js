825e3a287557ee343a4b269977dcbe1a43d68169
William Ambrose <W.P.Ambrose@gmail.com>
2015-01-13 16:58:38 -0500

    Working on calculator page

---
+++ calculator.js
@@ -0,0 +1,14 @@
+var rt = "Click or type to enter numbers and operators";
+
+var current = 0;
+
+function number_pressed(incoming)
+{
+	console.log("hey, " + incoming + " was pressed");
+}
+
+function decimal_pressed()
+{
+	// respond to user pressing decimal point/period key
+	console.log("hey, the decimal point/period key was pressed");
+}
