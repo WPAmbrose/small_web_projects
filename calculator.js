e3f4ad1f7a725baa99b00fa8f450d0ee98a796e6
William Ambrose <W.P.Ambrose@gmail.com>
2015-01-13 23:12:28 -0500

    Added some UI, functions, and functionality to calculator, WIP

--- calculator.js
+++ calculator.js
@@ -1,14 +1,83 @@
-var rt = "Click or type to enter numbers and operators";
-
+var rt = "Click to enter numbers and operators";
 var current = 0;
+var past_decimal = 0;
+var incoming = 0;
+var out = document.getElementById("out");
+
+out.innerHTML = rt;
+
+function update_display()
+{
+	// this updates the currently displayed number
+	out.innerHTML = current;
+}
+
+function clear_total()
+{
+	// respond to the C button
+	out.innerHTML = "Clearing...";
+	rt = 0;
+	current = 0;
+	past_decimal = 0;
+	update_display();
+}
 
 function number_pressed(incoming)
 {
-	console.log("hey, " + incoming + " was pressed");
+	// respond to number buttons
+	out.innerHTML = "hey, " + incoming + " was pressed";
+	/*
+	if (past_decimal = 0)
+	{
+		// this adjusts the current number to account for ordinary numeric input
+		current = current * 10 + incoming;
+	}
+	else
+	{
+		// this adjusts the current number to account for numeric input after the decimal point has been entered
+		// past_decimal (effectively) keeps track of how many times a number has been added after the decimal
+		// raising 1/10 to the power of the position past the decimal produces 0.1, 0.01, 0.001, etc.,
+		// allowing for easy scaling
+		current = current + (((1 / 10) ^ past_decimal) * incoming);
+	}
+	update_display();
+	*/
 }
 
 function decimal_pressed()
 {
-	// respond to user pressing decimal point/period key
-	console.log("hey, the decimal point/period key was pressed");
+	// respond to the decimal point button
+	past_decimal = 1;
+	out.innerHTML = "the decimal point button was pressed";
+}
+
+function division()
+{
+	// respond to the / button
+	out.innerHTML = "the / button was pressed";
+}
+
+function multiplication()
+{
+	// respond to the * button
+	out.innerHTML = "the * button was pressed";
+}
+
+function subtraction()
+{
+	// respond to the - button
+	out.innerHTML = "the - button was pressed";
+}
+
+function addition()
+{
+	// respond to the + button
+	out.innerHTML = "the + button was pressed";
+}
+
+function calculate()
+{
+	// this is called whenever an operator button is pressed,
+	// and is the only thing called when the = button is pressed
+	out.innerHTML = "the = button was pressed";
 }
